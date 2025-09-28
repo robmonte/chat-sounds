4babe5730e78edb00aaca0a6c7c4c83d4ed01f9f
robmonte <17119716+robmonte@users.noreply.github.com>
2025-09-28 04:18:07 -0500

    overhaul

--- src/main/java/com/chatsounds/ChatSoundsConfig.java
+++ src/main/java/com/chatsounds/ChatSoundsConfig.java
@@ -3,50 +3,197 @@ package com.chatsounds;
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
+import net.runelite.client.config.ConfigSection;
 import net.runelite.client.config.Range;
 import net.runelite.client.config.Units;
 
 @ConfigGroup("chatSounds")
 public interface ChatSoundsConfig extends Config
 {
+	/**************
+	* PUBLIC CHAT *
+ 	**************/
+	@ConfigSection(
+			name = "Public chat",
+			description = "Settings for public chat.",
+			position = 0
+	)
+	String publicList = "publicList";
+
 	@ConfigItem(
 		keyName = "publicChat",
 		name = "Public Chat",
 		description = "The sound effect to use when receiving a public chat message.",
-		position = 1
+		position = 1,
+		section = publicList
 	)
 	default ChatSoundsMode publicChat()
 	{
 		return ChatSoundsMode.OFF;
 	}
 
+	@Range(
+			max = 100
+	)
+	@Units(
+			value = "%"
+	)
+	@ConfigItem(
+			keyName = "publicVolume",
+			name = "Volume",
+			description = "Sets the volume of the chat message sound effect.",
+			position = 2,
+			section = publicList
+	)
+	default int publicVolume()
+	{
+		return 100;
+	}
+
+	@ConfigItem(
+			keyName = "publicIgnorePlayers",
+			name = "Ignore player names",
+			description = "A comma-separated list of player names to never play a sound for.",
+			position = 4,
+			section = publicList
+	)
+	default String publicIgnorePlayersList()
+	{
+		return "";
+	}
+
+	/***************
+	* PRIVATE CHAT *
+	****************/
+	@ConfigSection(
+			name = "Private chat",
+			description = "Settings for private chat.",
+			position = 5
+	)
+	String privateList = "privateList";
+
 	@ConfigItem(
 		keyName = "privateChat",
 		name = "Private Chat",
 		description = "The sound effect to use when receiving a private chat message.",
-		position = 2
+		position = 6,
+		section = privateList
 	)
 	default ChatSoundsMode privateChat()
 	{
 		return ChatSoundsMode.DEFAULT;
 	}
 
+	@Range(
+			max = 100
+	)
+	@Units(
+			value = "%"
+	)
+	@ConfigItem(
+			keyName = "privateVolume",
+			name = "Volume",
+			description = "Sets the volume of the chat message sound effect.",
+			position = 7,
+			section = privateList
+	)
+	default int privateVolume()
+	{
+		return 100;
+	}
+
+	@ConfigItem(
+			keyName = "privateIgnorePlayers",
+			name = "Ignore player names",
+			description = "A comma-separated list of player names to never play a sound for.",
+			position = 9,
+			section = privateList
+	)
+	default String privateIgnorePlayersList()
+	{
+		return "";
+	}
+
+	/****************
+ 	* CHAT-CHANNELS *
+	****************/
+	@ConfigSection(
+			name = "Chat-channel",
+			description = "Settings for chat-channels.",
+			position = 10
+	)
+	String chatChannelList = "chatChannelList";
+
 	@ConfigItem(
 		keyName = "chatChannel",
 		name = "Chat-channel",
 		description = "The sound effect to use when receiving a chat-channel chat message.",
-		position = 3
+		position = 11,
+		section = chatChannelList
 	)
 	default ChatSoundsMode chatChannel()
 	{
 		return ChatSoundsMode.OFF;
 	}
 
+	@Range(
+			max = 100
+	)
+	@Units(
+			value = "%"
+	)
+	@ConfigItem(
+			keyName = "chatChannelVolume",
+			name = "Volume",
+			description = "Sets the volume of the chat message sound effect.",
+			position = 12,
+			section = chatChannelList
+	)
+	default int chatChannelVolume()
+	{
+		return 100;
+	}
+
+	@ConfigItem(
+			keyName = "chatChannelIgnoreJoinLeave",
+			name = "Ignore joined/left messages",
+			description = "Do not play a sound for users joining or leaving chats.",
+			position = 13,
+			section = chatChannelList
+	)
+	default boolean chatChannelIgnoreJoinLeave()
+	{
+		return false;
+	}
+
+	@ConfigItem(
+			keyName = "chatChannelIgnorePlayers",
+			name = "Ignore player names",
+			description = "A comma-separated list of player names to never play a sound for.",
+			position = 14,
+			section = chatChannelList
+	)
+	default String chatChannelIgnorePlayersList()
+	{
+		return "";
+	}
+
+	/*************
+ 	* CLAN CHATS *
+ 	*************/
+	@ConfigSection(
+			name = "Clan chat",
+			description = "Settings for clan chats.",
+			position = 15
+	)
+	String clanChatList = "clanChatList";
+
 	@ConfigItem(
 		keyName = "clanChat",
 		name = "Clan Chat",
 		description = "The sound effect to use when receiving a clan chat message.",
-		position = 4
+		position = 16,
+		section = clanChatList
 	)
 	default ChatSoundsMode clanChat()
 	{
@@ -57,7 +204,8 @@ public interface ChatSoundsConfig extends Config
 		keyName = "clanBroadcast",
 		name = "Clan Broadcast",
 		description = "The sound effect to use when receiving a clan broadcast message.",
-		position = 5
+		position = 17,
+		section = clanChatList
 	)
 	default ChatSoundsMode clanBroadcast()
 	{
@@ -65,19 +213,184 @@ public interface ChatSoundsConfig extends Config
 	}
 
 	@Range(
-		max = 100
+			max = 100
 	)
 	@Units(
-		value = "%"
+			value = "%"
 	)
 	@ConfigItem(
-		keyName = "volume",
-		name = "Volume",
-		description = "Sets the volume of the chat message sound effect.",
-		position = 10
+			keyName = "clanVolume",
+			name = "Volume",
+			description = "Sets the volume of the chat message sound effect.",
+			position = 18,
+			section = clanChatList
 	)
-	default int volume()
+	default int clanVolume()
 	{
 		return 100;
+	}
+
+	@ConfigItem(
+			keyName = "clanIgnoreJoinLeave",
+			name = "Ignore joined/left messages",
+			description = "Do not play a sound for users joining or leaving chats.",
+			position = 19,
+			section = clanChatList
+	)
+	default boolean clanIgnoreJoinLeave()
+	{
+		return false;
+	}
+
+	@ConfigItem(
+			keyName = "clanIgnorePlayers",
+			name = "Ignore player names",
+			description = "A comma-separated list of player names to never play a sound for.",
+			position = 20,
+			section = clanChatList
+	)
+	default String clanIgnorePlayersList()
+	{
+		return "";
+	}
+
+	/**************
+ 	* GUEST CLANS *
+	**************/
+	@ConfigSection(
+			name = "Guest clan chat",
+			description = "Settings for guest clan chats.",
+			position = 21
+	)
+	String guestClanList = "guestClanList";
+
+	@ConfigItem(
+			keyName = "guestClanChat",
+			name = "Guest Clan Chat",
+			description = "The sound effect to use when receiving a guest clan chat message.",
+			position = 6,
+			section = guestClanList
+	)
+	default ChatSoundsMode guestClanChat()
+	{
+		return ChatSoundsMode.OFF;
+	}
+
+	@ConfigItem(
+			keyName = "guestClanBroadcast",
+			name = "Guest Clan Broadcast",
+			description = "The sound effect to use when receiving a guest clan broadcast message.",
+			position = 22,
+			section = guestClanList
+	)
+	default ChatSoundsMode guestClanBroadcast()
+	{
+		return ChatSoundsMode.OFF;
+	}
+
+	@Range(
+			max = 100
+	)
+	@Units(
+			value = "%"
+	)
+	@ConfigItem(
+			keyName = "guestClanChannelVolume",
+			name = "Volume",
+			description = "Sets the volume of the chat message sound effect.",
+			position = 23,
+			section = guestClanList
+	)
+	default int guestClanVolume()
+	{
+		return 100;
+	}
+
+	@ConfigItem(
+			keyName = "guestClanIgnoreJoinLeave",
+			name = "Ignore joined/left messages",
+			description = "Do not play a sound for users joining or leaving chats.",
+			position = 24,
+			section = guestClanList
+	)
+	default boolean guestClanIgnoreJoinLeave()
+	{
+		return false;
+	}
+
+	@ConfigItem(
+			keyName = "guestClanIgnorePlayers",
+			name = "Ignore player names",
+			description = "A comma-separated list of player names to never play a sound for.",
+			position = 25,
+			section = guestClanList
+	)
+	default String guestClanIgnorePlayersList()
+	{
+		return "";
+	}
+
+	/****************
+ 	* GROUP IRONMAN *
+	****************/
+	@ConfigSection(
+			name = "Group ironman chat",
+			description = "Settings for group ironman chats.",
+			position = 26
+	)
+	String groupList = "groupList";
+
+	@ConfigItem(
+		keyName = "gimChat",
+		name = "Group Ironman Chat",
+		description = "The sound effect to use when receiving a group ironman chat message.",
+		position = 27,
+		section = groupList
+	)
+	default ChatSoundsMode gimChat()
+	{
+		return ChatSoundsMode.OFF;
+	}
+
+	@ConfigItem(
+		keyName = "gimBroadcast",
+		name = "Group Ironman Broadcast",
+		description = "The sound effect to use when receiving a group ironman broadcast message.",
+		position = 28,
+		section = groupList
+	)
+	default ChatSoundsMode gimBroadcast()
+	{
+		return ChatSoundsMode.OFF;
+	}
+
+	@Range(
+			max = 100
+	)
+	@Units(
+			value = "%"
+	)
+	@ConfigItem(
+			keyName = "groupIronVolume",
+			name = "Volume",
+			description = "Sets the volume of the chat message sound effect.",
+			position = 29,
+			section = groupList
+	)
+	default int groupIronVolume()
+	{
+		return 100;
+	}
+
+	@ConfigItem(
+			keyName = "groupIronIgnorePlayers",
+			name = "Ignore player names",
+			description = "A comma-separated list of player names to never play a sound for.",
+			position = 31,
+			section = groupList
+	)
+	default String groupIronIgnorePlayersList()
+	{
+		return "";
 	}
 }
--- src/main/java/com/chatsounds/ChatSoundsPlugin.java
+++ src/main/java/com/chatsounds/ChatSoundsPlugin.java
@@ -6,6 +6,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.List;
 import javax.inject.Inject;
 import javax.swing.SwingUtilities;
 import lombok.extern.slf4j.Slf4j;
@@ -19,6 +20,7 @@ import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
+import net.runelite.client.util.Text;
 
 @Slf4j
 @PluginDescriptor(
@@ -26,21 +28,49 @@ import net.runelite.client.plugins.PluginDescriptor;
 )
 public class ChatSoundsPlugin extends Plugin
 {
-	private static final String CS_CLAN_MSG = "To talk in your clan's channel, start each line of chat with // or /c.";
+	private static final String DEFAULT_FILE_NAME = "cs_default.mp3";
+	private static final String PUBLIC_FILE_NAME = "cs_public.mp3";
+	private static final String PRIVATE_FILE_NAME = "cs_private.mp3";
+	private static final String CHAT_CHANNEL_FILE_NAME = "cs_chat_channel.mp3";
+	private static final String CLAN_FILE_NAME = "cs_clan.mp3";
+	private static final String CLAN_BROADCAST_FILE_NAME = "cs_clan_broadcast.mp3";
+	private static final String CLAN_GUEST_FILE_NAME = "cs_clan_guest.mp3";
+	private static final String CLAN_GUEST_BROADCAST_FILE_NAME = "cs_clan_guest_broadcast.mp3";
+	private static final String GIM_FILE_NAME = "cs_gim.mp3";
+	private static final String GIM_BROADCAST_FILE_NAME = "cs_gim_broadcast.mp3";
+
+	private static final String HAS_JOINED = " has joined.";
+	private static final String HAS_LEFT = " has left.";
+	private static final String CS_CHAT_CHANNEL_MSG_1 = "Attempting to join chat-channel...".toLowerCase();
+	private static final String CS_CHAT_CHANNEL_MSG_2 = "Now talking in chat-channel ".toLowerCase();
+	private static final String CS_CHAT_CHANNEL_MSG_3 = "To talk, start each line of chat with the / symbol.".toLowerCase();
+	private static final String CS_CLAN_MSG = "To talk in your clan's channel, start each line of chat with // or /c.".toLowerCase();
+	private static final String CS_CLAN_GUEST_MSG_1 = "You are now a guest of ".toLowerCase();
+	private static final String CS_CLAN_GUEST_MSG_2 = "To talk, start each line of chat with /// or /gc.".toLowerCase();
+	private static final String CS_GIM_MSG = "To talk in your Ironman Group's channel, start each line of chat with //// or /g.".toLowerCase();
+
 	private static final File CS_DIR = new File(RuneLite.RUNELITE_DIR.getPath() + File.separator + "chat-sounds");
-	private static final File CS_DEFAULT = new File(CS_DIR, "cs_default.mp3");
-	private static final File CS_PUBLIC = new File(CS_DIR, "cs_public.mp3");
-	private static final File CS_PRIVATE = new File(CS_DIR, "cs_private.mp3");
-	private static final File CS_CHAT_CHANNEL = new File(CS_DIR,"cs_chat_channel.mp3");
-	private static final File CS_CLAN = new File(CS_DIR, "cs_clan.mp3");
-	private static final File CS_CLAN_BROADCAST = new File(CS_DIR, "cs_clan_broadcast.mp3");
+	private static final File CS_DEFAULT = new File(CS_DIR, DEFAULT_FILE_NAME);
+	private static final File CS_PUBLIC = new File(CS_DIR, PUBLIC_FILE_NAME);
+	private static final File CS_PRIVATE = new File(CS_DIR, PRIVATE_FILE_NAME);
+	private static final File CS_CHAT_CHANNEL = new File(CS_DIR, CHAT_CHANNEL_FILE_NAME);
+	private static final File CS_CLAN = new File(CS_DIR, CLAN_FILE_NAME);
+	private static final File CS_CLAN_BROADCAST = new File(CS_DIR, CLAN_BROADCAST_FILE_NAME);
+	private static final File CS_CLAN_GUEST = new File(CS_DIR, CLAN_GUEST_FILE_NAME);
+	private static final File CS_CLAN_GUEST_BROADCAST = new File(CS_DIR, CLAN_GUEST_BROADCAST_FILE_NAME);
+	private static final File CS_GIM = new File(CS_DIR, GIM_FILE_NAME);
+	private static final File CS_GIM_BROADCAST = new File(CS_DIR, GIM_BROADCAST_FILE_NAME);
 	private static final File[] CS_FILES = new File[]{
 		CS_DEFAULT,
 		CS_PUBLIC,
 		CS_PRIVATE,
 		CS_CHAT_CHANNEL,
 		CS_CLAN,
-		CS_CLAN_BROADCAST
+		CS_CLAN_BROADCAST,
+		CS_CLAN_GUEST,
+		CS_CLAN_GUEST_BROADCAST,
+		CS_GIM,
+		CS_GIM_BROADCAST
 	};
 
 	@Inject
@@ -59,55 +89,114 @@ public class ChatSoundsPlugin extends Plugin
 	public void onChatMessage(ChatMessage event)
 	{
 		Player player = client.getLocalPlayer();
-		String eventName = removePrefixedImageFromName(event.getName());
-		if (player == null ||
-			client.getGameState() != GameState.LOGGED_IN ||
-			eventName.equals(player.getName()))
-		{
+		String playerName = player.getName() != null ? player.getName() : "";
+		String eventName = Text.sanitize(event.getName());
+		if (player == null || client.getGameState() != GameState.LOGGED_IN ||
+				eventName.equalsIgnoreCase(Text.sanitize(playerName))) {
 			return;
 		}
 
+		String msg = Text.standardize(event.getMessage());
 		ChatMessageType type = event.getType();
-		switch (type)
-		{
+		switch (type) {
 			case MODCHAT:
 			case PUBLICCHAT:
-				playSound(config.publicChat(), CS_PUBLIC);
+				if (shouldAlertOnPlayer(config.publicIgnorePlayersList(), eventName)) {
+					return;
+				}
+				playSound(config.publicChat(), CS_PUBLIC, config.publicVolume());
 				break;
+
 			case MODPRIVATECHAT:
 			case PRIVATECHAT:
-				playSound(config.privateChat(), CS_PRIVATE);
+				if (shouldAlertOnPlayer(config.privateIgnorePlayersList(), eventName)) {
+					return;
+				}
+				playSound(config.privateChat(), CS_PRIVATE, config.privateVolume());
 				break;
+
 			case FRIENDSCHAT:
-				playSound(config.chatChannel(), CS_CHAT_CHANNEL);
+				if (shouldAlertOnPlayer(config.chatChannelIgnorePlayersList(), eventName)) {
+					return;
+				}
+				playSound(config.chatChannel(), CS_CHAT_CHANNEL, config.chatChannelVolume());
 				break;
+
+			case FRIENDSCHATNOTIFICATION:
+				if (shouldAlertOnJoinOrLeft(msg, config.chatChannelIgnoreJoinLeave()) &&
+						!msg.equals(CS_CHAT_CHANNEL_MSG_1) && !msg.startsWith(CS_CHAT_CHANNEL_MSG_2) &&
+						!msg.equals(CS_CHAT_CHANNEL_MSG_3)) {
+					playSound(config.chatChannel(), CS_CHAT_CHANNEL, config.chatChannelVolume());
+				}
+				break;
+
 			case CLAN_CHAT:
-				playSound(config.clanChat(), CS_CLAN);
+				if (shouldAlertOnPlayer(config.clanIgnorePlayersList(), eventName)) {
+					return;
+				}
+				playSound(config.clanChat(), CS_CLAN, config.clanVolume());
 				break;
+
 			case CLAN_MESSAGE:
-				if (!event.getMessage().equals(CS_CLAN_MSG)) {
-					playSound(config.clanBroadcast(), CS_CLAN_BROADCAST);
+				if (shouldAlertOnJoinOrLeft(msg, config.clanIgnoreJoinLeave()) && !msg.equals(CS_CLAN_MSG)) {
+					playSound(config.clanBroadcast(), CS_CLAN_BROADCAST, config.clanVolume());
+				}
+				break;
+
+			case CLAN_GUEST_CHAT:
+				if (shouldAlertOnPlayer(config.guestClanIgnorePlayersList(), eventName)) {
+					return;
+				}
+				playSound(config.guestClanChat(), CS_CLAN_GUEST, config.guestClanVolume());
+				break;
+
+			case CLAN_GUEST_MESSAGE:
+				if (shouldAlertOnJoinOrLeft(msg, config.guestClanIgnoreJoinLeave()) &&
+						!msg.startsWith(CS_CLAN_GUEST_MSG_1) && !msg.endsWith(CS_CLAN_GUEST_MSG_2)) {
+					playSound(config.guestClanBroadcast(), CS_CLAN_GUEST_BROADCAST, config.guestClanVolume());
+				}
+				break;
+
+			case CLAN_GIM_CHAT:
+				if (shouldAlertOnPlayer(config.groupIronIgnorePlayersList(), eventName)) {
+					return;
+				}
+				playSound(config.gimChat(), CS_GIM, config.groupIronVolume());
+				break;
+
+			case CLAN_GIM_MESSAGE:
+				if (!msg.equals(CS_GIM_MSG)) {
+					playSound(config.gimBroadcast(), CS_GIM_BROADCAST, config.groupIronVolume());
 				}
 				break;
 		}
 	}
 
-	// Special accounts types have an icon prepended onto the chat event's name in certain chat channels.
-	// i.e. "<img=99>Username"
-	private String removePrefixedImageFromName(String text) {
-		boolean startsWithImgMarkup = text.startsWith("<img") && text.contains(">");
-		boolean hasTextAfterMarkup = text.indexOf(">") != text.length() - 1;
-		if (startsWithImgMarkup && hasTextAfterMarkup) {
-			text = text.substring(text.indexOf(">") + 1);
+	// Returns true if the message is from an ignored player in the chat's type.
+	private boolean shouldAlertOnPlayer(String ignoreList, String eventName) {
+		List<String> ignorePlayersList = Text.fromCSV(ignoreList.trim().toLowerCase());
+
+        return ignorePlayersList.contains(eventName.toLowerCase());
+    }
+
+	// Returns false if it is not a "join" or "left" message. If it is one, returns true if ignore is
+	// not checked, false if it is checked.
+	private boolean shouldAlertOnJoinOrLeft(String text, boolean ignore) {
+		if (!text.endsWith(HAS_JOINED) && !text.endsWith(HAS_LEFT)) {
+			return false;
 		}
-		return text;
-	}
+
+        return !ignore;
+    }
 
 	private void initSoundFiles()
 	{
 		if (!CS_DIR.exists())
 		{
-			CS_DIR.mkdirs();
+			boolean dirsResult = CS_DIR.mkdirs();
+			if (!dirsResult) {
+				log.info("Failed to create directory " + CS_DIR);
+			}
 		}
 
 		for (File f : CS_FILES)
@@ -117,7 +206,7 @@ public class ChatSoundsPlugin extends Plugin
 				if (f.exists()) {
 					continue;
 				}
-				InputStream stream = ChatSoundsPlugin.class.getClassLoader().getResourceAsStream("cs_default.mp3");
+				InputStream stream = ChatSoundsPlugin.class.getClassLoader().getResourceAsStream(DEFAULT_FILE_NAME);
 				OutputStream out = new FileOutputStream(f);
 				byte[] buffer = new byte[8 * 1024];
 				int bytesRead;
@@ -132,7 +221,7 @@ public class ChatSoundsPlugin extends Plugin
 		}
 	}
 
-	private void playSound(ChatSoundsMode mode, File f)
+	private void playSound(ChatSoundsMode mode, File f, int volume)
 	{
 		if (mode == ChatSoundsMode.OFF || !f.exists())
 		{
@@ -147,7 +236,7 @@ public class ChatSoundsPlugin extends Plugin
 				{
 					mp3Player = new MP3Player(f);
 				}
-				mp3Player.setVolume(config.volume());
+				mp3Player.setVolume(volume);
 				mp3Player.play();
 			});
 	}
